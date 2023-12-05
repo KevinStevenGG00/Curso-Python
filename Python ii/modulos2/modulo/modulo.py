a1e4ef857df1908980c321ad3e26bbc506674853
KevinWork <kevin.godoy.pe@gmail.com>
2023-12-05 14:07:54 -0500

    test3

--- Python ii/modulos2/modulo/modulo.py
+++ Python ii/modulos2/modulo/modulo.py
@@ -2,8 +2,8 @@
 
 import sys
 #agregar un modulo según su ubicación
-#sys.path.append("c:\\Users\\7408950068-PC\\Desktop\\Kevin\\PYTHON\\modulos2\\funciones")
-sys.path.append("C:\\Users\\740899500754-PC\\Desktop\\Kevin\\Curso-Python\\Python ii\\modulos2\\funciones")
+sys.path.append("c:\\Users\\7408950068-PC\\Desktop\\Kevin\\PYTHON\\modulos2\\funciones")
+#sys.path.append("C:\\Users\\740899500754-PC\\Desktop\\Kevin\\Curso-Python\\Python ii\\modulos2\\funciones")
 
 #imprimir todos los modulos
 print(sys.path)
